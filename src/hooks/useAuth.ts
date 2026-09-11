2bb3f5f847f52f9156eb8c575e528c23a570806b
NAVEEN KUMAR D <naveen28342@gmail.com>
2026-09-11 08:53:21 +0530

    Add files via upload

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -79,7 +79,7 @@ export function useAuth() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin
+        redirectTo: `${window.location.origin}/`
       }
     });
     if (error) {
